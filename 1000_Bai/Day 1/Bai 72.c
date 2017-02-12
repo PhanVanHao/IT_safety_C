283f470bddda4ac20a797e7edee6d5cac0890772
PhanVanHao <haouno@gmail.com>
2017-02-12 23:39:02 +0700

    UPLOAD

---
+++ 1000_Bai/Day 1/Bai 72.c
@@ -0,0 +1,13 @@
+gt(int n)
+{
+    if (n==0) return 1;
+    return gt(n-1)*n;
+}
+main()
+{
+    int n=5,i,x=4;
+    float t;
+    for (i=1;i<=n;i++)
+        t+=(float)pow(-1,n+1)*pow(x,n)/gt(n);
+    printf("KQ=%f",t);
+}
